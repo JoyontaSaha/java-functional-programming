893bab29d8b0abcd57e3fd53bf949ec07f2f38e7
Joyonta Saha <joyontasaha8@gmail.com>
2021-07-18 01:00:07 +0600

    predicate functional interface chaining with (and , or)

--- src/main/java/functionalinterface/_Predicate.java
+++ src/main/java/functionalinterface/_Predicate.java
@@ -14,12 +14,25 @@ public class _Predicate {
         System.out.println(isPhoneNumberValidPredicate.test("077777"));
         System.out.println(isPhoneNumberValidPredicate.test("0099999999"));
 
+        System.out.println("Declarative Approach ....chaining with and");
+        System.out.println(isPhoneNumberValidPredicate.and(containsNumber).test("0099999999"));
+
+        System.out.println("Declarative Approach ....chaining with or");
+        System.out.println(isPhoneNumberValidPredicate.or(containsNumber).test("0099999999"));
+
+
     }
 
     // Declarative Approach
     // Predicate Functional Interface
     // 1 argument 1 boolean return
     private static Predicate<String> isPhoneNumberValidPredicate  = phoneNumber -> phoneNumber.startsWith("00") && phoneNumber.length() == 10;
+
+
+    // Declarative Approach
+    // Predicate Functional Interface
+    // 1 argument 1 boolean return
+    private static Predicate<String> containsNumber  = phoneNumber -> phoneNumber.contains("3");
 
 
     // Imperative Approach
